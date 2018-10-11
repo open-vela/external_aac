0b52b209b1ae9302855c337f2ff950612f392768
liujinye <liujinye@xiaomi.com>
2018-10-11 23:10:04 +0800

    [automerger] DO NOT MERGE Prevent out of bounds accesses in lppTransposer() am: 2eaadebcb6 am: 589e1e861f am: e2491a2024 am: 9c00c42d95 skipped: c7e9a56431
    Change-Id: I53774f0e504aeb7ee31e260753542ba586f97e5d

--- libSBRdec/src/lpp_tran.cpp
+++ libSBRdec/src/lpp_tran.cpp
@@ -96,10 +96,6 @@ amm-info@iis.fraunhofer.de
   \sa lppTransposer(), main_audio.cpp, sbr_scale.h, \ref documentationOverview
 */
 
-#ifdef __ANDROID__
-#include <cutils/log.h>
-#endif
-
 #include "lpp_tran.h"
 
 #include "sbr_ram.h"
@@ -260,6 +256,7 @@ void lppTransposer (HANDLE_SBR_LPP_TRANS hLppTrans,    /*!< Handle of lpp transp
   int ovLowBandShift;
   int lowBandShift;
 /*  int ovHighBandShift;*/
+  int targetStopBand;
 
 
   alphai[0] = FL2FXCONST_SGL(0.0f);
@@ -276,32 +273,24 @@ void lppTransposer (HANDLE_SBR_LPP_TRANS hLppTrans,    /*!< Handle of lpp transp
 
   autoCorrLength = pSettings->nCols + pSettings->overlap;
 
-  if (pSettings->noOfPatches > 0) {
-    /* Set upper subbands to zero:
-       This is required in case that the patches do not cover the complete highband
-       (because the last patch would be too short).
-       Possible optimization: Clearing bands up to usb would be sufficient here. */
-    int targetStopBand = patchParam[pSettings->noOfPatches-1].targetStartBand
-                   + patchParam[pSettings->noOfPatches-1].numBandsInPatch;
+  /* Set upper subbands to zero:
+     This is required in case that the patches do not cover the complete highband
+     (because the last patch would be too short).
+     Possible optimization: Clearing bands up to usb would be sufficient here. */
+  targetStopBand = patchParam[pSettings->noOfPatches-1].targetStartBand
+                 + patchParam[pSettings->noOfPatches-1].numBandsInPatch;
 
-    int memSize = ((64) - targetStopBand) * sizeof(FIXP_DBL);
+  int memSize = ((64) - targetStopBand) * sizeof(FIXP_DBL);
 
-    if (!useLP) {
-      for (i = startSample; i < stopSampleClear; i++) {
-        FDKmemclear(&qmfBufferReal[i][targetStopBand], memSize);
-        FDKmemclear(&qmfBufferImag[i][targetStopBand], memSize);
-      }
-    } else
+  if (!useLP) {
     for (i = startSample; i < stopSampleClear; i++) {
       FDKmemclear(&qmfBufferReal[i][targetStopBand], memSize);
+      FDKmemclear(&qmfBufferImag[i][targetStopBand], memSize);
     }
+  } else
+  for (i = startSample; i < stopSampleClear; i++) {
+    FDKmemclear(&qmfBufferReal[i][targetStopBand], memSize);
   }
-#ifdef __ANDROID__
-  else {
-    // Safetynet logging
-    android_errorWriteLog(0x534e4554, "112160868");
-  }
-#endif
 
   /* init bwIndex for each patch */
   FDKmemclear(bwIndex, MAX_NUM_PATCHES*sizeof(INT));
