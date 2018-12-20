6042be9985c835bf6e7255b47ae2cf20c79cf36a
liujinye <liujinye@xiaomi.com>
2018-12-20 15:52:46 +0800

    Add sanity check in huff_decode()
    Test: atest DecoderTestXheAac ; atest DecoderTestAacDrc
    Bug: 119292397
    Change-Id: I33e99629665df9aa6262c90dd7ebdde4b4b9d773
    (cherry picked from commit b81f869de3f7c5b6395606d5f36cef57987eae8f)

--- libAACdec/src/aacdec_hcrs.cpp
+++ libAACdec/src/aacdec_hcrs.cpp
@@ -367,10 +367,7 @@ static UINT InitSegmentBitfield(UINT *pNumSegment,
   UINT tempWord;
   USHORT numValidSegment;
 
-  *pNumWordForBitfield =
-      (*pNumSegment == 0)
-          ? 0
-          : ((*pNumSegment - 1) >> THIRTYTWO_LOG_DIV_TWO_LOG) + 1;
+  *pNumWordForBitfield = ((*pNumSegment - 1) >> THIRTYTWO_LOG_DIV_TWO_LOG) + 1;
 
   /* loop over all words, which are completely used or only partial */
   /* bit in pSegmentBitfield is zero if segment is empty; bit in
