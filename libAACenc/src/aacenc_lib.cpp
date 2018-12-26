7fdce08954f4fe3423afb276e7a3df078d2b6d35
liujinye <liujinye@xiaomi.com>
2018-12-26 18:18:35 +0800

    Merge "Signed Integer Overflow in CLpd_FAC_Acelp2Mdct()" am: 3bb4fe68a6 am: 8879fb1fd6 am: 350e4ffea2
    Change-Id: I5f6e1d37824f13d791edd8433aac9c591bbfc399

--- libAACenc/src/aacenc_lib.cpp
+++ libAACenc/src/aacenc_lib.cpp
@@ -1733,10 +1733,9 @@ AACENC_ERROR aacEncEncode(const HANDLE_AACENCODER hAacEncoder,
   }
 
   /* check if buffer descriptors are filled out properly. */
-  if ((inargs == NULL) || (outargs == NULL) ||
-      ((AACENC_OK != validateBufDesc(inBufDesc)) &&
-       (inargs->numInSamples > 0)) ||
-      (AACENC_OK != validateBufDesc(outBufDesc))) {
+  if ((AACENC_OK != validateBufDesc(inBufDesc)) ||
+      (AACENC_OK != validateBufDesc(outBufDesc)) || (inargs == NULL) ||
+      (outargs == NULL)) {
     err = AACENC_UNSUPPORTED_PARAMETER;
     goto bail;
   }
