e3edafed11d10ca9f473d9b50eecd527654f2e8c
openvela-robot <openvela-robot@xiaomi.com>
2019-02-09 02:17:39 +0800

    Snap for 5180536 from 75987acc798db1fe03002d04500dac994eb6d20c to pi-platform-release
    Change-Id: I5ebe5584689c6ac68fa82bef53068d114d4fc91f

--- libSBRdec/src/env_extr.cpp
+++ libSBRdec/src/env_extr.cpp
@@ -1145,10 +1145,10 @@ static int sbrGetEnvelope(
   \brief    Generates frame info for FIXFIXonly frame class used for low delay
  version
 
-  \return   zero for error, one for correct.
+  \return   nothing
  ****************************************************************************/
-static int generateFixFixOnly(FRAME_INFO *hSbrFrameInfo, int tranPosInternal,
-                              int numberTimeSlots, const UINT flags) {
+static void generateFixFixOnly(FRAME_INFO *hSbrFrameInfo, int tranPosInternal,
+                               int numberTimeSlots, const UINT flags) {
   int nEnv, i, tranIdx;
   const int *pTable;
 
@@ -1159,11 +1159,12 @@ static int generateFixFixOnly(FRAME_INFO *hSbrFrameInfo, int tranPosInternal,
     case 15:
       pTable = FDK_sbrDecoder_envelopeTable_15[tranPosInternal];
       break;
+    default:
+      FDK_ASSERT(0);
+      /* fall through */
     case 16:
       pTable = FDK_sbrDecoder_envelopeTable_16[tranPosInternal];
       break;
-    default:
-      return 0;
   }
 
   /* look number of envelopes in table */
@@ -1186,8 +1187,6 @@ static int generateFixFixOnly(FRAME_INFO *hSbrFrameInfo, int tranPosInternal,
   /* nEnv is always > 1, so nNoiseEnvelopes is always 2 (IEC 14496-3 4.6.19.3.2)
    */
   hSbrFrameInfo->nNoiseEnvelopes = 2;
-
-  return 1;
 }
 
 /*!
@@ -1231,9 +1230,7 @@ static int extractLowDelayGrid(
   }
 
   /* calculate borders according to the transient position */
-  if (!generateFixFixOnly(pFrameInfo, temp, numberTimeSlots, flags)) {
-    return 0;
-  }
+  generateFixFixOnly(pFrameInfo, temp, numberTimeSlots, flags);
 
   /* decode freq res: */
   for (k = 0; k < pFrameInfo->nEnvelopes; k++) {
