fcb0468f11b25c3990782f9cd1ecf86519c4e439
openvela-robot <openvela-robot@xiaomi.com>
2020-05-11 08:21:35 +0800

    Set min_sdk_version to be part of mainline modules am: 71c477bae4
    Change-Id: I2fce92c77e6583c6662636a66e8a0e85c9fbbec4

--- libMpegTPEnc/src/tpenc_lib.cpp
+++ libMpegTPEnc/src/tpenc_lib.cpp
@@ -647,7 +647,7 @@ TRANSPORTENC_ERROR transportEnc_GetLibInfo(LIB_INFO *info) {
   info->module_id = FDK_TPENC;
   info->version = LIB_VERSION(TP_LIB_VL0, TP_LIB_VL1, TP_LIB_VL2);
   LIB_VERSION_STRING(info);
-#ifdef SUPPRESS_BUILD_DATE_INFO
+#ifdef __ANDROID__
   info->build_date = "";
   info->build_time = "";
 #else
