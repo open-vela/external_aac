de4c219e73b1fa9e6409caaf9220f9e86a8b6a22
liujinye <liujinye@xiaomi.com>
2020-05-29 23:49:24 +0800

    point to new repo hooks for mainline uploads am: ebc1030f65
    Change-Id: I2f08b01ee76f7524fb041d8541847eb9b4832f14

--- libAACdec/src/aacdecoder_lib.cpp
+++ libAACdec/src/aacdecoder_lib.cpp
@@ -122,7 +122,7 @@ amm-info@iis.fraunhofer.de
 #define AACDECODER_LIB_VL1 2
 #define AACDECODER_LIB_VL2 0
 #define AACDECODER_LIB_TITLE "AAC Decoder Lib"
-#ifdef SUPPRESS_BUILD_DATE_INFO
+#ifdef __ANDROID__
 #define AACDECODER_LIB_BUILD_DATE ""
 #define AACDECODER_LIB_BUILD_TIME ""
 #else
