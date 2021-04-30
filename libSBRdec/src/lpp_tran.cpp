bbd383dbbf51d65a872fb05272d417a5b22c7dd2
liujinye <liujinye@xiaomi.com>
2021-04-30 22:21:28 +0800

    Merge "Prevent signed integer overflow in complex path of calc_qmfBuffer()." into sc-dev am: dff30034b9
    Original change: https://googleplex-android-review.googlesource.com/c/platform/external/aac/+/14387482
    
    Change-Id: I9895067d725d564d1c9708cd8c6a921cd94b25aa

--- libSBRdec/src/lpp_tran.cpp
+++ libSBRdec/src/lpp_tran.cpp
@@ -1,7 +1,7 @@
 /* -----------------------------------------------------------------------------
 Software License for The Fraunhofer FDK AAC Codec Library for Android
 
-© Copyright  1995 - 2020 Fraunhofer-Gesellschaft zur Förderung der angewandten
+© Copyright  1995 - 2019 Fraunhofer-Gesellschaft zur Förderung der angewandten
 Forschung e.V. All rights reserved.
 
  1.    INTRODUCTION
@@ -216,21 +216,19 @@ static inline void calc_qmfBufferReal(FIXP_DBL **qmfBufferReal,
                                       const FIXP_DBL *const lowBandReal,
                                       const int startSample,
                                       const int stopSample, const UCHAR hiBand,
-                                      const int dynamicScale,
+                                      const int dynamicScale, const int descale,
                                       const FIXP_SGL a0r, const FIXP_SGL a1r) {
-  const int dynscale = fixMax(0, dynamicScale - 1) + 1;
-  const int rescale = -fixMin(0, dynamicScale - 1) + 1;
-  const int descale =
-      fixMin(DFRACT_BITS - 1, LPC_SCALE_FACTOR + dynamicScale + rescale);
+  FIXP_DBL accu1, accu2;
+  int i;
 
-  for (int i = 0; i < stopSample - startSample; i++) {
-    FIXP_DBL accu;
+  for (i = 0; i < stopSample - startSample; i++) {
+    accu1 = fMultDiv2(a1r, lowBandReal[i]);
+    accu1 = (fMultDiv2(a0r, lowBandReal[i + 1]) + accu1);
+    accu1 = accu1 >> dynamicScale;
 
-    accu = fMultDiv2(a1r, lowBandReal[i]) + fMultDiv2(a0r, lowBandReal[i + 1]);
-    accu = (lowBandReal[i + 2] >> descale) + (accu >> dynscale);
-
-    qmfBufferReal[i + startSample][hiBand] =
-        SATURATE_LEFT_SHIFT(accu, rescale, DFRACT_BITS);
+    accu1 <<= 1;
+    accu2 = (lowBandReal[i + 2] >> descale);
+    qmfBufferReal[i + startSample][hiBand] = accu1 + accu2;
   }
 }
 
@@ -812,7 +810,9 @@ void lppTransposer(
           FDK_ASSERT(dynamicScale >= 0);
           calc_qmfBufferReal(
               qmfBufferReal, &(lowBandReal[LPC_ORDER + startSample - 2]),
-              startSample, stopSample, hiBand, dynamicScale, a0r, a1r);
+              startSample, stopSample, hiBand, dynamicScale,
+              fMin(DFRACT_BITS - 1, (LPC_SCALE_FACTOR + dynamicScale)), a0r,
+              a1r);
         }
       } /* bw <= 0 */
 
