33750ba47745fb35e76cefb1bc01594107769143
shipei <shipei@xiaomi.com>
2023-11-15 19:40:55 +0800

    aac codec:move ver info to include header file
    move lib ver info to header file for ffmpeg libfdk_aac dec
    
    Signed-off-by: shipei <shipei@xiaomi.com>

--- libAACdec/include/aacdecoder_lib.h
+++ libAACdec/include/aacdecoder_lib.h
@@ -432,6 +432,20 @@ Where N equals to CStreamInfo::frameSize .
 #include "FDK_audio.h"
 
 #include "genericStds.h"
+
+/* Decoder library info */
+#define AACDECODER_LIB_VL0 3
+#define AACDECODER_LIB_VL1 2
+#define AACDECODER_LIB_VL2 0
+#define AACDECODER_LIB_TITLE "AAC Decoder Lib"
+#ifdef SUPPRESS_BUILD_DATE_INFO
+#define AACDECODER_LIB_BUILD_DATE ""
+#define AACDECODER_LIB_BUILD_TIME ""
+#else
+#define AACDECODER_LIB_BUILD_DATE __DATE__
+#define AACDECODER_LIB_BUILD_TIME __TIME__
+#endif
+
 /**
  * \brief  AAC decoder error codes.
  */
